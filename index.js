96d7d117355b817add9fb9afc0bacbc797886db6
Gustavo <lg.santos2007@gmail.com>
2024-11-05 09:02:50 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const personagem = 'arqueiro';
+
+const classificacao = 20000;
+
+
+if (classificacao < 1000){
+
+    console.log('Sua classificação é ferro');
+
+}else if(classificacao >=1001 && classificacao <= 2000){
+    
+    console.log('Sua classificação é Bronze');
+
+}else if(classificacao >=2001 && classificacao <= 5000){
+    
+    console.log('Sua classificação é Prata');
+
+}else if(classificacao >=5001 && classificacao <= 7000){
+    
+    console.log('Sua classificação é Ouro');
+
+}else if(classificacao >=7001 && classificacao <= 8000){
+    
+    console.log('Sua classificação é platina');
+
+}else if(classificacao >=8001 && classificacao <= 9000){
+    
+    console.log('Sua classificação é ascedente');
+    
+}else if(classificacao >=9001 && classificacao <= 10000){
+    
+    console.log('Sua classificação é Imortal');
+
+}else if(classificacao >=10001){
+    
+    console.log('Sua classificação é radiante');
+
+}
+
